9051079dbc2ad5d18771fd0c3483a09107d97ad8
Renato Lira <renatolira1012@gmail.com>
2022-03-23 06:35:31 -0300

    Site de exemplo finalizado

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let form = document.getElementById("contact-form");
+let formName = document.getElementById("form-name");
+
+function callForm() {
+    form.className = "show-form";
+    formName.focus();
+}
